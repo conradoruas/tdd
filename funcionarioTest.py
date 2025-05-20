b633b8214f02a08b34c25a200dabda586c2f336f
conradoruas <conradoruas99@gmail.com>
2025-05-20 19:32:11 -0300

    Teste 2 - Funcionario

--- funcionarioTest.py
+++ funcionarioTest.py
@@ -10,6 +10,14 @@ class TestFuncionario(unittest.TestCase):
         self.assertEqual(self.funcionario.nome, "João Silva")
         self.assertEqual(self.funcionario.cargo, "Desenvolvedor")
         self.assertEqual(self.funcionario.projetos, [])
+    
+    def teste2_altera_nome_funcionario(self):
+        self.funcionario.setNome("Abraão Medeiros")
+
+        self.assertEqual(self.funcionario.id, 1)
+        self.assertEqual(self.funcionario.nome, "Abraão Medeiros")
+        self.assertEqual(self.funcionario.cargo, "Desenvolvedor")
+        self.assertEqual(self.funcionario.projetos, [])
 
 if __name__ == "__main__":
     unittest.main()
