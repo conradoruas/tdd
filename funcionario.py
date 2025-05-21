3a43feae346c31b4d20ccf8ce432eddfc96a29c3
conradoruas <conradoruas99@gmail.com>
2025-05-21 00:54:04 -0300

    Implementação 5 e 6 - Ocorrencia

--- funcionario.py
+++ funcionario.py
@@ -4,6 +4,7 @@ class Funcionario:
         self.nome = nome
         self.cargo = cargo
         self.projetos = []
+        self.ocorrencias = []
     
     def setNome(self, nome):
         self.nome = nome
@@ -16,3 +17,20 @@ class Funcionario:
 
     def removeProjeto(self, projeto):
         self.projetos.remove(projeto)
+    
+    def addOcorrencia(self, ocorrencia):
+        if self.hasLimiteDeOcorrencias():
+            raise ValueError("Funcionário atingiu o limite de 10 ocorrencias")
+        self.ocorrencias.append(ocorrencia)
+
+    def removeOcorrencia(self, ocorrencia):
+        self.ocorrencias.remove(ocorrencia)
+
+    def hasLimiteDeOcorrencias(self):
+        count = sum(1 for ocorrencia in self.ocorrencias if ocorrencia.estado != "fechada")
+        return count >= 10
+    
+    def getOcorrenciaByChave(self, chave):
+        for ocorrencia in self.ocorrencias:
+            if chave == ocorrencia.chave:
+                return ocorrencia
--- ocorrencia.py
+++ ocorrencia.py
@@ -14,4 +14,10 @@ class Ocorrencia:
     def mudar_responsavel(self, novo_responsavel):
         if self.estado == "fechada":
             raise ValueError("Não é possível mudar responsável de ocorrência fechada")
+
+        if novo_responsavel.hasLimiteDeOcorrencias():
+            raise ValueError("Não é possível adicionar uma ocorrencia a um responsável que já tem 10 ocorrencias")
+        
+        self.responsavel.removeOcorrencia(self)
+        novo_responsavel.addOcorrencia(self)
         self.responsavel = novo_responsavel
