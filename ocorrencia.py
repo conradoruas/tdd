314faf2cee4dbee554755251c4a516e7294926bd
conradoruas <conradoruas99@gmail.com>
2025-05-20 21:54:50 -0300

    Implementação 1 - Ocorrencia

---
+++ ocorrencia.py
@@ -0,0 +1,9 @@
+class Ocorrencia:
+    def __init__(self, chave, resumo, tipo, prioridade, projeto, responsavel):
+        self.chave = chave
+        self.resumo = resumo
+        self.tipo = tipo
+        self.prioridade = prioridade
+        self.projeto = projeto
+        self.responsavel = responsavel
+        self.estado = "aberta"
