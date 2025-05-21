d885b914d818dc5657a127dd2df18fd92c2fa8c8
conradoruas <conradoruas99@gmail.com>
2025-05-20 21:06:58 -0300

    Implementação 4 e 5 - Empresa

--- empresa.py
+++ empresa.py
@@ -8,3 +8,7 @@ class Empresa:
     def addFuncionarios(self, funcionarios: list):
         for funcionario in funcionarios:
             self.funcionarios.append(funcionario)
+    
+    def addProjetos(self, projetos: list):
+        for projeto in projetos:
+            self.projetos.append(projeto)
